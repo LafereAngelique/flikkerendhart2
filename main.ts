a2f2586f2881bb322aa5db1191907bf967f09f32
LafereAngelique <122604814+LafereAngelique@users.noreply.github.com>
2023-01-13 11:29:36 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.forever(function () {
+    for (let index = 0; index < 10000000; index++) {
+        basic.showLeds(`
+            . # . # .
+            # # # # #
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        basic.clearScreen()
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    }
+})
